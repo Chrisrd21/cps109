80acc817ac3f8a6eea22223eabcd1df1e3adf37e
Chrisrd21 <150264256+Chrisrd21@users.noreply.github.com>
2024-03-15 09:44:21 -0400

    Add files via upload

---
+++ assignments/ceasar_cypher.py
@@ -0,0 +1,16 @@
+plaintext_message = input('Enter a message: ')
+secret_key = int(input('Enter a number from 1-26 for the secret key: '))
+
+for char in plaintext_message:
+    if char.isalpha():
+        if char.isupper():
+            start = ord('A')
+        else:
+            start = ord('a')
+            new_pos = (ord(char) - start + secret_key) % 26 + start
+            new_char = chr(new_pos)
+            ciphertext += new_char
+    else:
+        ciphertext += char
+
+print(f'The ciphertext is : {ciphertext}')
